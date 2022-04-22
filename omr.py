2e0eaaeb6961d86bd48185ca1aa440226994539c
Akshit17 <akshit.chodhary@gmail.com>
2022-04-23 00:00:09 +0530

    applied canny edge detection and visualised contours

--- omr.py
+++ omr.py
@@ -1,48 +1,70 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
 
-while True:
-    _, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+img_path = 'bubblesheet_1.jpg'
+img = cv2.imread(img_path)
 
-    # hue and sat value for hsv img
-    lower_red = np.array([0, 50, 25])   
-    upper_red = np.array([255, 255, 180])
+print(img.shape)            # Original size is 1600 * 1200 and 3 color channels
+img_width = 500
+img_height = 500
+img = cv2.resize(img, (img_width, img_height))
 
-    mask = cv2.inRange(hsv, lower_red, upper_red)  # masking
-    cv2.imshow('mask', mask)
+# print(cv2.getGaussianKernel())
 
-    res = cv2.bitwise_and(frame, frame, mask=mask)  
-    cv2.imshow('res', res)
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)      # grayscaled image
+img_blur = cv2.GaussianBlur(img_gray, (5,5), 1)      # blurred image
+                        # (inputimage, kernel size , sigma_x)          when only sigma_x is provived sigma_y is taken as the same as sigma_x  (sigma_x is Gaussian kernel standard deviation in X direction)
+                        #https://docs.opencv.org/4.x/d4/d13/tutorial_py_filtering.html
+cv2.imshow('Blurred', img_blur)
 
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:      # key 27 is ESC
-        break
+img_canny = cv2.Canny(img_blur, 100, 200)                         # Edge detection on processed image
+                 #  ( input_img, Lower Threshold, Upper threshold)
+                    #  https://docs.opencv.org/3.4/da/d22/tutorial_py_canny.html
+
+cv2.imshow('Edge detection', img_canny)
+
+contours = cv2.findContours(img_canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)   # Find contours
+
+img_contours = img.copy()
+cv2.drawContours(img_contours, contours[0], -1, (0,255,0), 3)  #parameters are (image, contours,contour_color, contour_thickness)
+cv2.imshow('Contours', img_contours)
+
+cv2.imshow('Original', img)
 
 
-
-
-
-
-
-    # img_path = 'bubblesheet_1.jpg'
-    # img = cv2.imread(img_path)
-
-    # print(img.shape)            # Original size is 1600 * 1200 and 3 color channels
-    # img_width = 500
-    # img_height = 500
-    # img = cv2.resize(img, (img_width, img_height))
-
-    # print(cv2.getGaussianKernel())
-
-    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)      # grayscaled image
-    # img_blur = cv2.GaussianBlur(img_gray, (5,5), 1)      # blurred image
-    #                         # (inputimage, kernel size , sigma_x)          when only sigma_x is provived sigma_y is taken as the same as sigma_x  (sigma_x is Gaussian kernel standard deviation in X direction)
-    #                         #https://docs.opencv.org/4.x/d4/d13/tutorial_py_filtering.html
+cv2.waitKey(0)
 
 
 
-    # cv2.imshow('Original', img)
-    # cv2.waitKey(0)
+
+# cap = cv2.VideoCapture(0)
+
+# while True:
+#     _, frame = cap.read()
+#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+#     # hue and sat value for hsv img
+#     lower_red = np.array([0, 50, 25])   
+#     upper_red = np.array([255, 255, 180])
+
+#     mask = cv2.inRange(hsv, lower_red, upper_red)  # masking
+#     cv2.imshow('mask', mask)
+
+#     res = cv2.bitwise_and(frame, frame, mask=mask)  
+
+#     smoothed = cv2.filter2D(res, -1, np.ones((15, 15), np.float32)/225)
+#     cv2.imshow('smoothed', smoothed)
+
+#     blur = cv2.GaussianBlur(res, (15, 15), 0)
+#     cv2.imshow('blur', blur)
+
+#     median = cv2.medianBlur(res, 15)
+
+#     cv2.imshow('res', res)
+
+#     cv2.imshow('frame', frame)
+
+#     k = cv2.waitKey(5) & 0xFF
+#     if k == 27:      # key 27 is ESC
+#         break
