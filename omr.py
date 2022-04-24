13f6a4b9026b4c046913d32cdc709bfc16382b83
Akshit17 <akshit.chodhary@gmail.com>
2022-04-24 23:59:44 +0530

    Segmenting section containing bubbles (SSCB) attempt1

--- omr.py
+++ omr.py
@@ -1,24 +1,58 @@
 import cv2
 import numpy as np
 
+#TODO: Abstract the rectangular area containing the bubbles from the image
+
+def extract_rect(contours):                          #Needs optimization as it does not work for all bubblesheet examples
+    for c in contours:
+        if  cv2.contourArea(c) > 5000:
+            perimeter = cv2.arcLength(c, True)  
+            approx = cv2.approxPolyDP(c, 0.02*perimeter, True) 
+            x, y, w, h = cv2.boundingRect(approx)
+
+    # Corner points of the rectangle further used to be used to wrap the rectangular section
+    point_1 = np.array([x, y])
+    point_2 = np.array([x+w, y])
+    point_3 = np.array([x, y+h])
+    point_4 = np.array([w, h])
+
+    corner_list = []
+    corner_list.append(point_1)
+    corner_list.append(point_2)
+    corner_list.append(point_3)
+    corner_list.append(point_4)
+
+    return corner_list
+
+
+
+#TODO: Find the bubbles and draw them on the image
+def find_bubbles(contours):
+    bubbles = []
+    for i in range(len(contours)):
+        area = cv2.contourArea(contours[i])
+        if area > 1000:
+            bubbles.append(contours[i])
+    return bubbles
+
 
 # img_path = 'bubblesheet_1.jpg'
 # img = cv2.imread(img_path)
 
-# img_path = 'bubblesheet_2.jpg'
-# img = cv2.imread(img_path)
+img_path = 'bubblesheet_2.jpg'
+img = cv2.imread(img_path)
 
 # img_path = 'bubblesheet_3.jpg'
 # img = cv2.imread(img_path)
 
-img_path = 'bubblesheet_4.jpg'
-img = cv2.imread(img_path)
+# img_path = 'bubblesheet_4.jpg'
+# img = cv2.imread(img_path)
 
 
 print(img.shape)            # Original size is 1600 * 1200 and 3 color channels
-img_width = 1020
-img_height = 1020
-img = cv2.resize(img, (img_width, img_height))
+img_width = 1000
+img_height = 1000
+img = cv2.resize(img, (img_width, img_height), interpolation=cv2.INTER_AREA)
 
 
 
@@ -28,24 +62,25 @@ img_blur = cv2.GaussianBlur(img_gray, (5,5), 0)      # blurred image
                         #https://docs.opencv.org/4.x/d4/d13/tutorial_py_filtering.html
 cv2.imshow('Blurred', img_blur)
 
-img_canny = cv2.Canny(img_blur, 10, 100)                         # Edge detection on processed image using Canny edge detection , binary thresholding could have been an alternative (i.e  If the pixel value is smaller than the threshold, it is set to 0, otherwise it is set to a maximum value. )
-                 #  ( input_img, Lower Threshold, Upper threshold)
+img_canny = cv2.Canny(img_blur, 20, 60)                         # Edge detection on processed image using Canny edge detection , binary thresholding could have been an alternative (i.e  If the pixel value is smaller than the threshold, it is set to 0, otherwise it is set to a maximum value. )
+                 #  ( input_img, Lower Threshold, Upper threshold)       
                     #  https://docs.opencv.org/3.4/da/d22/tutorial_py_canny.html
 
 cv2.imshow('Edge detection', img_canny)
 
-contours, heirarchy = cv2.findContours(img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)   # Find contours. 
+contours, heirarchy = cv2.findContours(img_canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)   # Find contours. 
                             #parameters are (input_image, retrieval_mode, approximation_method)
-
-                     
+           
 img_contours = img.copy()
 cv2.drawContours(img_contours, contours, -1, (0,255,0), 1)  #parameters are (image, contours, countour_idx, contour_color, contour_thickness) . contour_idx is -1 for all contours
 cv2.imshow('Contours', img_contours)
 
+img_thresh  = cv2.threshold(img_gray, 170, 255, cv2.THRESH_BINARY_INV)[1]
+cv2.imshow('Threshold', img_thresh)
+
 cv2.imshow('Original', img)
 
 
-#TODO: Find the bubbles and draw them on the image
 
 #TODO: Implement logic for marks counting
 cv2.waitKey(0)
